753d18c4a1c0505d85ca08a1b0e7e90733dd50c4
corvin-rose <business@corvin-rose.de>
2023-02-19 00:32:44 +0100

    PKMN-39: Integrated Queue for BasketItem

--- backend/src/main/java/de/htwberlin/port/basket/consumer/BasketConsumer.java
+++ backend/src/main/java/de/htwberlin/port/basket/consumer/BasketConsumer.java
@@ -1,17 +1,26 @@
 package de.htwberlin.port.basket.consumer;
 
+import de.htwberlin.core.appservice.mapper.IBasketItemMapper;
+import de.htwberlin.core.domain.service.IBasketService;
 import de.htwberlin.port.dto.ProductExportDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Slf4j
 @Service
 public class BasketConsumer {
+
+  private final IBasketService basketService;
+  private final IBasketItemMapper basketItemMapper;
 
   @RabbitListener(queues = {"product"})
   public void consume(ProductExportDto product) {
     log.info(
         String.format("Received a BasketItem from Product Queue <- %s", product.getPokemonId()));
+
+    basketService.addItemToBasket(basketItemMapper.toBasketItem(product));
   }
 }
