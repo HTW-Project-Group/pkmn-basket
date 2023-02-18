3ae2527f503af6a63061e26edde825095b6bf843
corvin-rose <business@corvin-rose.de>
2023-02-18 18:35:44 +0100

    PKMN-43: Implemented BasketController

--- backend/src/main/java/de/htwberlin/port/basket/BasketController.java
+++ backend/src/main/java/de/htwberlin/port/basket/BasketController.java
@@ -1,7 +1,13 @@
 package de.htwberlin.port.basket;
 
+import de.htwberlin.core.domain.model.Basket;
+import de.htwberlin.core.domain.model.BasketItem;
 import de.htwberlin.core.domain.service.IBasketService;
+import de.htwberlin.port.exception.BasketItemNotFoundException;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -10,5 +16,17 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:3000")
 public class BasketController {
 
-  private final IBasketService productService;
+  private final IBasketService basketService;
+
+  @GetMapping("/{id}")
+  public ResponseEntity<BasketItem> getBasketItem(@PathVariable("id") UUID id) {
+    return new ResponseEntity<>(
+        basketService.findBasketItemById(id).orElseThrow(BasketItemNotFoundException::new),
+        HttpStatus.OK);
+  }
+
+  @GetMapping("/user/{userId}")
+  public ResponseEntity<Basket> getBasketForUser(@PathVariable("userId") UUID userId) {
+    return new ResponseEntity<>(basketService.findBasketByUserId(userId), HttpStatus.OK);
+  }
 }
