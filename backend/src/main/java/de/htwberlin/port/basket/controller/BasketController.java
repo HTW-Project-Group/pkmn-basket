4007ed630caeaa8d51666730d5fd6cee8291da20
Alexander Mai <83711446+GodPepe7@users.noreply.github.com>
2023-02-20 16:02:38 +0100

    PKMN-55 cors fix

--- backend/src/main/java/de/htwberlin/port/basket/controller/BasketController.java
+++ backend/src/main/java/de/htwberlin/port/basket/controller/BasketController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1/basket")
-@CrossOrigin(origins = "http://localhost:3000")
 public class BasketController {
 
   private final IBasketService basketService;
--- backend/src/main/java/de/htwberlin/port/basket/controller/QueueController.java
+++ backend/src/main/java/de/htwberlin/port/basket/controller/QueueController.java
@@ -1,12 +1,10 @@
 package de.htwberlin.port.basket.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1/queue")
-@CrossOrigin(origins = "http://localhost:3000")
 public class QueueController {}
